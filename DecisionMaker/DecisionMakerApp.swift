26ef4c4361b94931a9750fcf5770f7e8ae186843
Emma Sinclair <beampacer@gmail.com>
2023-06-17 09:24:59 -0700

    Initial Commit

---
+++ DecisionMaker/DecisionMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecisionMakerApp.swift
+//  DecisionMaker
+//
+//  Created by Emma Sinclair on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct DecisionMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
