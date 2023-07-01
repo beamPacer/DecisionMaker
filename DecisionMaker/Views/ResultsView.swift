b087a5ec00fd297c1bea727eda4638e76a3a42c3
Emma Sinclair <beampacer@gmail.com>
2023-06-30 20:31:57 -0700

    Refactors ResultsView a little

--- DecisionMaker/Views/ResultsView.swift
+++ DecisionMaker/Views/ResultsView.swift
@@ -13,58 +13,71 @@ struct ResultsView: View {
 	var body: some View {
 		GeometryReader { geometry in
 			switch results.count {
-			case 0:
-				VStack {
+			case 0:		noResultsView
+			case 1:		oneResultView
+			default:	properResultsView
+			}
+		}
+	}
+	
+	var noResultsView: some View {
+		GeometryReader { geometry in
+			VStack {
+				Spacer()
+				HStack {
 					Spacer()
-					HStack {
-						Spacer()
-						Text(Strings.Result.noItemsTitle)
-							.frame(width: geometry.size.width * 0.75)
-							.font(.system(size: 22))
-							.multilineTextAlignment(.center)
-						Spacer()
-					}
-					Spacer()
-				}
-			case 1:
-				VStack {
-					Spacer()
-					HStack {
-						Spacer()
-						Text(Strings.Result.oneItemTitle)
-							.frame(width: geometry.size.width * 0.75)
-							.font(.system(size: 22))
-							.multilineTextAlignment(.center)
-						Spacer()
-					}
+					Text(Strings.Result.noItemsTitle)
+						.frame(width: geometry.size.width * 0.75)
+						.font(.system(size: 22))
+						.multilineTextAlignment(.center)
 					Spacer()
 				}
-			default:
-				VStack {
-					if let firstResult = results.first {
-						Text(firstResult.option.title)
-							.font(.system(size: 32))
-							.multilineTextAlignment(.center)
-							.padding()
-						Text(formatPercentWeightedAverage(for: firstResult))
-					}
-					
-					List {
-						Section(header: Text(Strings.Result.runnersUpTitle)) {
-							ForEach(results.dropFirst(), id: \.self) { result in
-								HStack {
-									Text(result.option.title)
-									Spacer()
-									Text(formatPercentWeightedAverage(for: result))
-										.foregroundColor(.gray)
-								}
-							}
+				Spacer()
+			}
+		}
+	}
+	
+	var oneResultView: some View {
+		GeometryReader { geometry in
+			VStack {
+				Spacer()
+				HStack {
+					Spacer()
+					Text(Strings.Result.oneItemTitle)
+						.frame(width: geometry.size.width * 0.75)
+						.font(.system(size: 22))
+						.multilineTextAlignment(.center)
+					Spacer()
+				}
+				Spacer()
+			}
+		}
+	}
+	
+	var properResultsView: some View {
+		VStack {
+			if let firstResult = results.first {
+				Text(firstResult.option.title)
+					.font(.system(size: 32))
+					.multilineTextAlignment(.center)
+					.padding()
+				Text(formatPercentWeightedAverage(for: firstResult))
+			}
+			
+			List {
+				Section(header: Text(Strings.Result.runnersUpTitle)) {
+					ForEach(results.dropFirst(), id: \.self) { result in
+						HStack {
+							Text(result.option.title)
+							Spacer()
+							Text(formatPercentWeightedAverage(for: result))
+								.foregroundColor(.gray)
 						}
 					}
 				}
-				.navigationTitle(Strings.Result.title)
 			}
 		}
+		.navigationTitle(Strings.Result.title)
 	}
 	
 	func formatPercentWeightedAverage(for result: Result) -> String {
