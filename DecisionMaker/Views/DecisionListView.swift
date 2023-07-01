3367f7dc703d98f762653435d795383c44287979
Emma Sinclair <beampacer@gmail.com>
2023-06-30 20:24:13 -0700

    Splits out DecisionListView into its own file

---
+++ DecisionMaker/Views/DecisionListView.swift
@@ -0,0 +1,62 @@
+//
+//  DecisionListView.swift
+//  DecisionMaker
+//
+//  Created by Emma Sinclair on 6/30/23.
+//
+
+import SwiftUI
+
+struct DecisionListView: View {
+	@EnvironmentObject var decisionData: DecisionData
+	@State private var newDecisionTitle: String = ""
+	@State private var isShowingNewDecisionView = false
+	
+	var body: some View {
+		NavigationView {
+			List {
+				ForEach(decisionData.decisions.indices, id: \.self) { index in
+					NavigationLink(destination: DecisionView(decision: $decisionData.decisions[index])) {
+						Text(decisionData.decisions[index].title)
+					}
+				}
+				.onDelete { indexSet in
+					decisionData.decisions.remove(atOffsets: indexSet)
+				}
+				
+				Button(action: {
+					isShowingNewDecisionView = true
+				}) {
+					Label(Strings.Decision.addNewTitle, systemImage: "plus")
+				}
+				.sheet(isPresented: $isShowingNewDecisionView) {
+					NavigationView {
+						VStack {
+							TextField(Strings.Decision.enterNewPrompt, text: $newDecisionTitle)
+								.textFieldStyle(.roundedBorder)
+								.padding()
+							
+							Button(action: {
+								let newDecision = Decision()
+								newDecision.title = newDecisionTitle
+								decisionData.decisions.append(newDecision)
+								isShowingNewDecisionView = false
+								newDecisionTitle = ""
+							}) {
+								Text(Strings.Common.done)
+									.font(.title2)
+									.fontWeight(.bold)
+									.padding()
+									.background(Color.blue)
+									.foregroundColor(.white)
+									.cornerRadius(10)
+									.padding()
+							}
+						}
+					}
+				}
+			}
+			.navigationBarTitle(Strings.Decision.mainListTitle)
+		}
+	}
+}
--- DecisionMaker/Views/DecisionView.swift
+++ DecisionMaker/Views/DecisionView.swift
@@ -7,60 +7,6 @@
 
 import SwiftUI
 
-struct DecisionListView: View {
-	@EnvironmentObject var decisionData: DecisionData
-	@State private var newDecisionTitle: String = ""
-	@State private var isShowingNewDecisionView = false
-	
-	var body: some View {
-		NavigationView {
-			List {
-				ForEach(decisionData.decisions.indices, id: \.self) { index in
-					NavigationLink(destination: DecisionView(decision: $decisionData.decisions[index])) {
-						Text(decisionData.decisions[index].title)
-					}
-				}
-				.onDelete { indexSet in
-					decisionData.decisions.remove(atOffsets: indexSet)
-				}
-				
-				Button(action: {
-					isShowingNewDecisionView = true
-				}) {
-					Label(Strings.Decision.addNewTitle, systemImage: "plus")
-				}
-				.sheet(isPresented: $isShowingNewDecisionView) {
-					NavigationView {
-						VStack {
-							TextField(Strings.Decision.enterNewPrompt, text: $newDecisionTitle)
-								.textFieldStyle(.roundedBorder)
-								.padding()
-							
-							Button(action: {
-								let newDecision = Decision()
-								newDecision.title = newDecisionTitle
-								decisionData.decisions.append(newDecision)
-								isShowingNewDecisionView = false
-								newDecisionTitle = ""
-							}) {
-								Text(Strings.Common.done)
-									.font(.title2)
-									.fontWeight(.bold)
-									.padding()
-									.background(Color.blue)
-									.foregroundColor(.white)
-									.cornerRadius(10)
-									.padding()
-							}
-						}
-					}
-				}
-			}
-			.navigationBarTitle(Strings.Decision.mainListTitle)
-		}
-	}
-}
-
 struct DecisionView: View {
 	@Binding var decision: Decision
 	@State private var isShowingNewAttributeView: Bool = false
