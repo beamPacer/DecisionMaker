a80c1cfd43d6b887494da66751847d99c759f403
Emma Sinclair <beampacer@gmail.com>
2023-07-02 08:04:46 +0100

    Integrates script to cloc for AI stats
    Written by #chatgpt #gpt4 after some back and forth

---
+++ scripts/clocWithKeywords.py
@@ -0,0 +1,47 @@
+# Written by chatGPT 4
+
+import subprocess
+
+def count_lines_of_change_with_keywords(repo_path, keywords):
+    command = ['git', '-C', repo_path, 'log', '--grep']
+    command.extend(keywords)
+    command.extend(['--pretty=format:""', '--numstat'])
+
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.DEVNULL, shell=False, universal_newlines=True)
+        lines_of_change = output.strip().split('\n')
+        line_count = 0
+
+        for line in lines_of_change:
+            if line.strip():
+                fields = line.strip().split('\t')
+                if len(fields) >= 2:
+                    added, deleted = fields[:2]
+                    line_count += int(added) + int(deleted)
+
+        return line_count
+    except subprocess.CalledProcessError:
+        return 0
+
+import subprocess
+
+def count_lines_of_change_all_time(repo_path):
+    command = ['git', '-C', repo_path, 'log', '--pretty=format:""', '--numstat']
+
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.DEVNULL, shell=False, universal_newlines=True)
+        lines_of_change = output.strip().split('\n')
+        line_count = 0
+
+        for line in lines_of_change:
+            if line.strip():
+                fields = line.strip().split('\t')
+                if len(fields) >= 2:
+                    added, deleted = fields[:2]
+                    line_count += int(added) if added != '-' else 0
+                    line_count += int(deleted) if deleted != '-' else 0
+
+        return line_count
+    except subprocess.CalledProcessError:
+        return 0
+
