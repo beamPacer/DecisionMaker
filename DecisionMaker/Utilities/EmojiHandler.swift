6389709e231a98d576fc83b26f58a94ef901ceac
Emma Sinclair <beampacer@gmail.com>
2023-07-04 01:27:39 +0100

    Removes (?) results from emojis search
    - Also extends unicode ranges for character search
    - I got the ranges from gpt 4, and the other work from SO. Not going to tag this commit.

--- DecisionMaker/Utilities/EmojiHandler.swift
+++ DecisionMaker/Utilities/EmojiHandler.swift
@@ -15,19 +15,28 @@ struct EmojiHandler {
 	
 	init() {
 		let emojiRanges = [
-			0x1F601...0x1F64F, // Emoticons
-			0x1F680...0x1F6C0, // Transport and Map Symbols
+			0x1F600...0x1F64F, // Emoticons
+			0x1F300...0x1F5FF, // Miscellaneous Symbols and Pictographs
+			0x1F680...0x1F6FF, // Transport and Map Symbols
+			0x1F700...0x1F77F, // Alchemical Symbols
+			0x1F780...0x1F7FF, // Geometric Shapes Extended
+			0x1F800...0x1F8FF, // Supplemental Arrows-C
+			0x1F900...0x1F9FF, // Supplemental Symbols and Pictographs
+			0x1FA00...0x1FA6F, // Chess Symbols
+			0x1FA70...0x1FAFF, // Symbols and Pictographs Extended-A
 			0x2600...0x26FF,   // Miscellaneous Symbols
 			0x2700...0x27BF,   // Dingbats
-			0x1F300...0x1F5FF, // Miscellaneous Symbols and Pictographs
-			0x1F900...0x1F9FF, // Supplemental Symbols and Pictographs
-			0x1F1E6...0x1F1FF  // Flags (iOS 9.0 and later)
+			0x1F1E6...0x1F1FF, // Flags (iOS 9.0 and later)
 		]
 
 		for range in emojiRanges {
 			for unicode in range {
 				if let scalar = UnicodeScalar(unicode) {
 					let emoji = String(scalar)
+					guard Character(emoji).unicodeAvailable() else {
+						continue
+					}
+					
 					let cfstr = NSMutableString(string: emoji) as CFMutableString
 					var range = CFRangeMake(0, CFStringGetLength(cfstr))
 					CFStringTransform(cfstr, &range, kCFStringTransformToUnicodeName, false)
